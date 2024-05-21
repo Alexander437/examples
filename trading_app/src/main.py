bcec249840bd8ccd7cd1ac37ffd364fa1e0db8b3
Alex <example@abc.com>
2024-05-21 12:33:54 +0300

    Redis for cache

--- trading_app/src/main.py
+++ trading_app/src/main.py
@@ -5,9 +5,13 @@
 # http://127.0.0.1:8000/docs
 # http://127.0.0.1:8000/redoc
 # docker run -p 5432:5432 --name pg_trading -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=postgres -d postgres:13.3
+# docker run -p 6379:6379 --name redis -d redis:latest
 # docker run -e 'PGADMIN_DEFAULT_EMAIL=admin@admin.admin' -e 'PGADMIN_DEFAULT_PASSWORD=admin' -d dpage/pgadmin4
 from fastapi import FastAPI
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
 
+from redis import asyncio as aioredis
 from auth.auth import auth_backend
 from auth.base_config import fastapi_users
 from auth.schemas import UserCreate, UserRead
@@ -30,3 +34,9 @@ app.include_router(
 )
 
 app.include_router(router_operations)
+
+
+@app.on_event("startup")
+async def startup_event():
+    redis = aioredis.from_url("redis://172.17.0.3", encoding="utf-8", decode_responses=True)
+    FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache")
--- trading_app/src/operations/router.py
+++ trading_app/src/operations/router.py
@@ -1,4 +1,7 @@
+import time
+
 from fastapi import APIRouter, Depends, HTTPException
+from fastapi_cache.decorator import cache
 from sqlalchemy import select, insert
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -10,6 +13,13 @@ router = APIRouter(
     prefix="/operations",
     tags=["Operations"],
 )
+
+
+@router.get("/long_operation")
+@cache(expire=30)
+def get_long_op():
+    time.sleep(2)
+    return "Длинная операция"
 
 
 @router.get("/")
